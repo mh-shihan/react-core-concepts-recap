e1b3c1c3a307f1af6873cc2fdb4a400b72f35bce
MHShihan <mahmodul.no.1@gmail.com>
2025-02-16 16:55:13 +0600

    Fetch data from publics folder json file

---
+++ src/components/student/Student.jsx
@@ -0,0 +1,6 @@
+const Student = ({ studentsList }) => {
+  const { id, name, age, student_id, section, university } = studentsList;
+  return <div>I am from Student</div>;
+};
+
+export default Student;
--- src/components/students/Students.jsx
+++ src/components/students/Students.jsx
@@ -1,7 +1,17 @@
-const Students = () => {
+import Student from "../student/student";
+
+const Students = ({ studentsList = [] }) => {
+  const { id, name, age, student_id, section, university } = studentsList;
   return (
     <div>
-      <h2 className="text-5xl font-bold">This Students Section</h2>
+      <h1 className="text-3xl text-center pt-4 font-bold">
+        Total Students {studentsList.length}
+      </h1>
+      <div>
+        {studentsList.map((student) => (
+          <Student key={student.id}></Student>
+        ))}
+      </div>
     </div>
   );
 };
